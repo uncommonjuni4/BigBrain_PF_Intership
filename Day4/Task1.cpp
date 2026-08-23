4d17f823d7986464ade148771d0fa216746b5226
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-08-23 17:14:00 +0500

    Day for code
    Day for code

---
+++ Day4/Task1.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std ;   //for use input and output libary functions 
+int main(){
+	
+//	with for loop
+
+//    for(int i  = 1 ; i<=10;i++){
+//    	cout<<"Big Brain Module "<<i<<endl;
+//	}
+	int i = 1;
+	
+//	with  while lopp
+	
+	while(i<=10){
+		cout<<"Big Brain Module "<<i<<endl;
+		i++;
+	}
+//	//with  do while loop
+//	do{
+//		cout<<"Big Brain Module "<<i<<endl;
+//		i++;
+//	}while(i<=10);
+
+
+  // print form 1 - 100
+//  with for loop
+   for(int i = 1 ; i<=100;i++){
+   	cout<<"Number  "<<i<<endl;
+   }
+    // with while loop 
+//   int i = 1;
+//   
+//   while(i<=100){
+//   	cout<<"Number = "<<i<<endl;
+//   	i++;
+//   }
+//   
+//   
+////   with do while lopp 
+//{
+//   	cout<<"Number = "<<i<<endl;
+//   	i++;
+//   }while(i<100);
+   
+   
+    return 0 ;
+}
+ 
